93f51b018a47d5e61eb68784c0e331307acc84ce
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-04-15 19:58:56 -0500

    Fi

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,17 +4,20 @@ folder of your AirBnB Clone repo, using the function do_pack'''
 from fabric.api import local, put, run, env
 from os import path
 from datetime import datetime
+
 env.hosts = ["{}@35.231.233.84".format(env.user),
              "{}@34.207.112.220".format(env.user)]
 
+env.tgz = ""
 
 def do_pack():
-    '''Packing web_static to versions/web_static_20170314233357.tgz'''
+    """ Create directory if doesn't exist and backup folder web_static """
+    time = str(datetime.now()).split(".")[0].replace(
+        ":", "").replace(" ", "").replace("-", "")
     if not path.exists("versions"):
-        local("mkdir versions")
-    local("tar -czf versions/web_static_{}.tgz web_static".
-          format(str(datetime.now()).split(".")[0].replace("-", "").
-                 replace(" ", "").replace(":", "")))
+        local("mkdir -p versions")
+    local("tar -czf versions/web_static_{}.tgz web_static".format(time))
+    env.tgz = ("versions/web_static_{}.tgz".format(time))
 
 
 def do_deploy(archive_path):
@@ -56,3 +59,10 @@ def do_deploy(archive_path):
 
     except:
         return(False)
+
+
+def deploy():
+    '''creates and distributes an archive to your web servers'''
+    if env.tgz == "":
+        do_pack()
+    return(do_deploy(env.tgz))

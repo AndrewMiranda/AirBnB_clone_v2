7bfb2dba5a6cdb2950ffae75ac55deb5349cfde2
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-05-05 14:05:04 -0500

    8Citi

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -2,37 +2,24 @@
 """ Starts Flask!!! """
 from flask import Flask, render_template
 from models import storage
-from models import State, City
+from models.state import State
+
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def close_storage(self):
-    """ Removing SQLAlchemy Session """
+def teardown(self):
+    """remove the current sqlalchemy session"""
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
-
-
-@app.route("/cities_by_states", strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    state_obj = storage.all("State")
-    city_obj = storage.all("City")
-    states = list()
-    cities = list()
-    for state, value in state_obj.items():
-        states.append(value)
-    for city, value in city_obj.items():
-        cities.append(value)
-    return render_template("8-cities_by_states.html",
-                           states=states,
-                           cities=cities)
+    """list all cities by state"""
+    states = list(storage.all('State').values())
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)

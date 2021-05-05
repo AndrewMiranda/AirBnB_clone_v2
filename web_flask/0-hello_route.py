4522a2480b88de3b3b233e79e9a236b3d25f51f3
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-05-05 11:29:17 -0500

    Fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Start Flask web application"""
-
+""" Starts Flask. """
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
+
+@app.route('/', strict_slashes=False)
 def hello_HBNB():
-    """To display a message"""
+    """ Hello HBNB!!! """
     return "Hello HBNB!"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-        
+
+if __name__ == '__main__':
+    app.run()
